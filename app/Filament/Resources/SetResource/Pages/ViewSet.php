371a073ad45dbfcee7f6a5e7d92b2c1c6d8ba0b4
Dave Mills <d.e.mills@stats4sd.org>
2024-02-24 17:01:13 +0000

    add set page and filter

---
+++ app/Filament/Resources/SetResource/Pages/ViewSet.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\SetResource\Pages;
+
+use App\Filament\Resources\SetResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewSet extends ViewRecord
+{
+    protected static string $resource = SetResource::class;
+}
